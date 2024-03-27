37db897184d60ef2da1af9d92f671feca47436dd
Marcus Yong <marcus.yong@motional.com>
2024-03-27 12:35:22 -0400

    missing vulkan

--- extra/adapter/build.sh
+++ extra/adapter/build.sh
@@ -22,7 +22,7 @@ echo COMPILER:$COMPILER
 COMMON_FLAGS="-g"
 DEBUG_FLAGS="-O0 -D_DEBUG"
 RELEASE_FLAGS="-O2"
-INCLUDE_FLAGS="-I../../../deps/imgui/imgui -I.../../../deps/glfw/include"
+INCLUDE_FLAGS="-I../../../deps/imgui/imgui -I../../../deps/glfw/include"
 
 if [ "$release" -eq "1" ]; then
    echo "Release enabled"
@@ -34,7 +34,8 @@ fi
 [ "$debug" -eq "1" ] && echo "Debug enabled" && eval "TARGET_FLAGS=\"$DEBUG_FLAGS\""
 
 cd build
-$COMPILER $COMMON_FLAGS $TARGET_FLAGS $INCLUDE_FLAGS -c ../*.cpp
+$COMPILER $COMMON_FLAGS $TARGET_FLAGS $INCLUDE_FLAGS -c ../imgui_impl_glfw.cpp
+$COMPILER $COMMON_FLAGS $TARGET_FLAGS $INCLUDE_FLAGS -c ../imgui_impl_vulkan.cpp
 wait
 ar rvs adapter.a *.o
 
