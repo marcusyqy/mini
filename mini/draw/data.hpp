ae0b0e61712c8e62c2d0ec47fa6c173b69fc776d
Marcus Yong <marcusyqy@gmail.com>
2024-04-13 22:58:17 -0400

    adding glm?

--- mini/draw/data.hpp
+++ mini/draw/data.hpp
@@ -1,3 +1,12 @@
 #pragma once
-// @TODO: change to volk instead.
+/// @TODO: change to volk instead.
 #include <vulkan/vulkan.h>
+#include "glm.hpp"
+
+
+/// @TODO: do we need double?
+using Vec2 = glm::vec2;
+using Vec3 = glm::vec3;
+using Vec4 = glm::vec4;
+
+// let's try to do some math ourselves but use glm first.
