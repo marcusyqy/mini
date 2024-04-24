6305541dccd3f1d81bc2a655c9eb26eb1d09ab61
Marcus Yong <marcusyqy@gmail.com>
2024-04-24 01:30:43 -0400

    interesting things with imgui

--- mini/main.cpp
+++ mini/main.cpp
@@ -236,9 +236,6 @@ int main(int, char**) {
   log_debug("debug");
   log_warn("warn");
 
-  bool show_demo_window    = true;
-  bool show_another_window = false;
-  ImVec4 clear_color       = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
   char* gradient_comp_shader_buffer    = nullptr;
   u64 gradient_comp_buffer_size = read_file(temp_allocator, &gradient_comp_shader_buffer, "kernel/gradient.comp.spv");
@@ -560,43 +557,16 @@ int main(int, char**) {
 
     // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to
     // learn more about Dear ImGui!).
+    static bool show_demo_window    = true;
     if (show_demo_window) ImGui::ShowDemoWindow(&show_demo_window);
 
-    // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
-    {
-      static float f     = 0.0f;
-      static int counter = 0;
-
-      ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!" and append into it.
-
-      ImGui::Text("This is some useful text.");          // Display some text (you can use a format strings too)
-      ImGui::Checkbox("Demo Window", &show_demo_window); // Edit bools storing our window open/close state
-      ImGui::Checkbox("Another Window", &show_another_window);
-
-      ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-      ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-      if (ImGui::Button("Button")) // Buttons return true when clicked (most widgets return true when edited/activated)
-        counter++;
-      ImGui::SameLine();
-      ImGui::Text("counter = %d", counter);
-
-      ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-      ImGui::End();
-    }
-
-    // 3. Show another simple window.
-    if (show_another_window) {
-      ImGui::Begin("Another Window", &show_another_window); // Pass a pointer to our bool variable (the window will have
-                                                            // a closing button that will clear the bool when clicked)
-      ImGui::Text("Hello from another window!");
-      if (ImGui::Button("Close Me")) show_another_window = false;
-      ImGui::End();
-    }
+    static bool show_metric_window = true;
+    if (show_metric_window) ImGui::ShowMetricsWindow(&show_metric_window);
 
 		
+    static bool show_background_window = true;
     static int current_background_effect = 0;
-		if (ImGui::Begin("background")) {
+		if (show_background_window && ImGui::Begin("background", &show_background_window)) {
 			Compute_Effect& selected = background_effects[current_background_effect];
 			ImGui::Text("Selected effect: %s", selected.name);
 			ImGui::SliderInt("Effect Index", &current_background_effect, 0, ARRAY_SIZE(background_effects) - 1);
@@ -604,9 +574,8 @@ int main(int, char**) {
 			ImGui::SliderFloat4("data2",(float*)& selected.data.data2, 0.0, 1.0);
 			ImGui::SliderFloat4("data3",(float*)& selected.data.data3, 0.0, 1.0);
 			ImGui::SliderFloat4("data4",(float*)& selected.data.data4, 0.0, 1.0);
-		
-			ImGui::End();
 		}
+    ImGui::End();
 
     ImGui::Render();
 
